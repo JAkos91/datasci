ffcf778c650fb334078a4a6ac1caed131faf5559
root <akos.juhasz.etu@univ-lille.fr>
2021-04-28 13:54:37 +0000

    sidebar-update

--- app.py
+++ app.py
@@ -4,25 +4,29 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
-data = pd.read_csv('data/csv')
+@st.cache
+def load_data():
+    data = pd.read_csv('https://opendata.ecdc.europa.eu/covid19/nationalcasedeath/csv')
+    data['week'] = data.year_week.apply(lambda x: convert(x))
+    return data
 
 def convert(x):
     year, week = x.split('-')
     year = (int(year) - 2020) * 54 
     return year + int(week)
-
-data['week'] = data.year_week.apply(lambda x: convert(x))
-
-hun = data[data.country == 'Hungary']
-
-fig = px.line(data_frame = hun, x = 'week', y = 'cumulative_count', color = 'indicator', )
+data = load_data()
 
 
+st.title("Daily number of new reported COVID-19 cases and deaths")
+st.text(" \n")
 
-#st.write("Again!")
+countries = data['country'].unique()
+country_choice = st.sidebar.selectbox('Select the country:', countries)
+country = data[data.country == country_choice]
 
-st.title("Hello World")
-country = st.selectbox('Select a country', ['Hungary', 'Belgium'])
-st.write(f"The selected country is: {country}")
+st.write(f"The selected country is: {country_choice}")
+
+fig = px.line(data_frame = country, x = 'week', y = 'cumulative_count', color = 'indicator', )
+
 st.plotly_chart(fig)
 
